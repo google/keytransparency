5a29c4fce85d8c1da5b77af89695888afb20f84f
Cesar Ghali <cesarghali@google.com>
2015-07-13 16:05:49 -0700

    Make HTTP Server Skeleton Tests More Robust
     - Unit test ParseUserId
     - Use table driven tests (e.g., https://talks.golang.org/2014/testing.slide#5)

--- rest/rest_test.go
+++ rest/rest_test.go
@@ -71,54 +71,120 @@ func TestFoo(t *testing.T) {
 }
 
 func TestGetUser_InitiateHandlerInfo(t *testing.T) {
-	email := "e2eshare.test@gmail.com"
-	appId := "gmail"
-	tm := time.Now().Format(time.RFC3339)
-	path := "/v1/users/" + email + "?appId=" + appId + "&time=" + tm
-
-	rInfo := handlers.RouteInfo{
-		path,
-		2,
-		"GET",
-		Fake_Initializer,
-		Fake_RequestHandler,
-	}
-	info := GetUser_InitializeHandlerInfo(rInfo)
-
-	switch info.Arg.(type) {
-	case *v2pb.GetUserRequest:
-		break
-	default:
-		t.Errorf("info.Arg is not of type v2pb.GetUserRequest")
+	var tests = []struct {
+		userId       string
+		appId        string
+		tm           string
+		isOutTimeNil bool
+		outSeconds   int64
+		outNanos     int
+	}{
+		{"e2eshare.test@gmail.com", "gmail", time.Now().Format(time.RFC3339), false, time.Now().Unix(), 0},
+		{"e2eshare.test@gmail.com", "", time.Now().Format(time.RFC3339), false, time.Now().Unix(), 0},
+		// time.Time{} is default value of time.Time
+		{"e2eshare.test@gmail.com", "gmail", time.Time{}.Format(time.RFC3339), true, 0, 0},
+		{"e2eshare.test@gmail.com", "", time.Time{}.Format(time.RFC3339), true, 0, 0},
 	}
 
-	u, err := url.Parse(path)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, test := range tests {
+		path := "/v1/users/" + test.userId
+		if (test.appId != "" && test.tm != time.Time{}.Format(time.RFC3339)) {
+			path += "?appId=" + test.appId + "&time=" + test.tm
+		} else if (test.appId == "" && test.tm != time.Time{}.Format(time.RFC3339)) {
+			path += "?time=" + test.tm
+		} else if (test.appId != "" && test.tm == time.Time{}.Format(time.RFC3339)) {
+			path += "?appId=" + test.appId
+		}
 
-	err = info.Parser(u, &info.Arg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if got, want := info.Arg.(*v2pb.GetUserRequest).UserId, email; got != want {
-		t.Errorf("UserId = %v, want %v", got, want)
-	}
-	if got, want := info.Arg.(*v2pb.GetUserRequest).AppId, appId; got != want {
-		t.Errorf("AppId = %v, want %v", got, want)
-	}
-	tt, _ := time.Parse(time.RFC3339, tm)
-	if gots, gotn, wants, wantn := info.Arg.(*v2pb.GetUserRequest).Time.Seconds, info.Arg.(*v2pb.GetUserRequest).Time.Nanos, tt.Unix(), tt.Nanosecond(); gots != wants || gotn != int32(wantn) {
-		t.Errorf("Time = %v [sec] %v [nsec], want %v [sec] %v [nsec]", gots, gotn, wants, wantn)
-	}
+		rInfo := handlers.RouteInfo{
+			path,
+			2,
+			"GET",
+			Fake_Initializer,
+			Fake_RequestHandler,
+		}
+		info := GetUser_InitializeHandlerInfo(rInfo)
 
-	v1 := &FakeServer{}
-	srv := New(v1)
-	resp, err := info.H(srv, nil, nil)
-	if err != nil {
-		t.Errorf("Error while calling Fake_RequestHandler, this should not happen.")
+		switch info.Arg.(type) {
+		case *v2pb.GetUserRequest:
+			break
+		default:
+			t.Errorf("info.Arg is not of type v2pb.GetUserRequest")
+		}
+
+		u, err := url.Parse(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = info.Parser(u, &info.Arg)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got, want := info.Arg.(*v2pb.GetUserRequest).UserId, test.userId; got != want {
+			t.Errorf("UserId = %v, want %v", got, want)
+		}
+		if got, want := info.Arg.(*v2pb.GetUserRequest).AppId, test.appId; got != want {
+			t.Errorf("AppId = %v, want %v", got, want)
+		}
+		if test.isOutTimeNil == false {
+			if gots, gotn, wants, wantn := info.Arg.(*v2pb.GetUserRequest).Time.Seconds, info.Arg.(*v2pb.GetUserRequest).Time.Nanos, test.outSeconds, test.outNanos; gots != wants || gotn != int32(wantn) {
+				t.Errorf("Time = %v [sec] %v [nsec], want %v [sec] %v [nsec]", gots, gotn, wants, wantn)
+			}
+		} else {
+			if info.Arg.(*v2pb.GetUserRequest).Time != nil {
+				t.Errorf("Time must be nil and it's not")
+			}
+		}
+
+		v1 := &FakeServer{}
+		srv := New(v1)
+		resp, err := info.H(srv, nil, nil)
+		if err != nil {
+			t.Errorf("Error while calling Fake_RequestHandler, this should not happen.")
+		}
+		if got, want := (*resp).(bool), true; got != want {
+			t.Errorf("resp = %v, want %v.", got, want)
+		}
 	}
-	if got, want := (*resp).(bool), true; got != want {
-		t.Errorf("resp = %v, want %v.", got, want)
+}
+
+func TestParseUserId(t *testing.T) {
+	var tests = []struct {
+		comp  []string
+		index int
+		out   string
+	}{
+		{[]string{"e2eshare.test@gmail.com"}, 0, "e2eshare.test@gmail.com"},
+		{[]string{"e2eshare.test@cs.ox.ac.uk"}, 0, "e2eshare.test@cs.ox.ac.uk"},
+		{[]string{"20"}, 0, ""},
+		{[]string{"-20"}, 0, ""},
+		{[]string{"+20"}, 0, ""},
+		{[]string{"20.2"}, 0, ""},
+		{[]string{"-20.2"}, 0, ""},
+		{[]string{"20.2"}, 0, ""},
+		{[]string{".2"}, 0, ""},
+		{[]string{"-.2"}, 0, ""},
+		{[]string{"+.2"}, 0, ""},
+		{[]string{"20.2.2"}, 0, "20.2.2"},
+		{[]string{"2E10"}, 0, ""},
+		{[]string{"+2E10"}, 0, ""},
+		{[]string{"-2E10"}, 0, ""},
+		{[]string{"2E+10"}, 0, ""},
+		{[]string{"2E-10"}, 0, ""},
+		{[]string{"2e10"}, 0, ""},
+		{[]string{"+2e10"}, 0, ""},
+		{[]string{"-2e10"}, 0, ""},
+		{[]string{"2e+10"}, 0, ""},
+		{[]string{"2e-10"}, 0, ""},
+		{[]string{"2E"}, 0, "2E"},
+		{[]string{"2e"}, 0, "2e"},
+	}
+	for _, test := range tests {
+		got, _ := parseUserId(test.comp, test.index)
+		want := test.out
+		if got != want {
+			t.Errorf("Error while parsing User ID. Input = (%v, %v), got '%v', want '%v'", test.comp, test.index, got, want)
+		}
 	}
 }
