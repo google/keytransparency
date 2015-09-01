a1e8591f05c702ab04a4e5390e80042362949304
Cesar Ghali <cesarghali@google.com>
2015-09-01 14:09:01 -0700

    More Fine Grained common Packages
      - Separate all common merkle tree code into its own sub package.

--- common/common.go
+++ common/common.go
@@ -21,7 +21,6 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"crypto/sha512"
-	"encoding/binary"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -31,23 +30,7 @@ import (
 )
 
 const (
-	// HashSize contains the blocksize of the used hash function in bytes.
-	HashSize = sha256.Size
-	// IndexLen is the maximum number of levels in this Merkle Tree.
-	IndexLen = HashSize * 8
-	// commitmentKeyLen is the number of bytes required to be in the
-	// profile commitment.
 	commitmentKeyLen = 16
-)
-
-var (
-	// TreeNonce is a constant value used as a salt in all leaf node calculations.
-	// The TreeNonce prevents different realms from producing collisions.
-	TreeNonce = []byte{241, 71, 100, 55, 62, 119, 69, 16, 150, 179, 228, 81, 34, 200, 144, 6}
-	// LeafIdentifier is the data used to indicate a leaf node.
-	LeafIdentifier = []byte("L")
-	// EmptyIdentifier is used while calculating the data of nil sub branches.
-	EmptyIdentifier = []byte("E")
 )
 
 // Commitment returns the commitment key and the profile commitment
@@ -72,37 +55,6 @@ func VerifyCommitment(key []byte, profile []byte, commitment []byte) error {
 		return grpc.Errorf(codes.InvalidArgument, "Invalid profile commitment")
 	}
 	return nil
-}
-
-// HashLeaf calculate the merkle tree leaf node value. This is computed as
-// H(TreeNonce || Identifier || depth || index || dataHash), where TreeNonce,
-// Identifier, depth, and index are fixed-length.
-func HashLeaf(identifier []byte, depth int, index []byte, dataHash []byte) []byte {
-	bdepth := make([]byte, 4)
-	binary.BigEndian.PutUint32(bdepth, uint32(depth))
-
-	h := sha256.New()
-	h.Write(TreeNonce)
-	h.Write(identifier)
-	h.Write(bdepth)
-	h.Write(index)
-	h.Write(dataHash)
-	return h.Sum(nil)
-}
-
-// HashIntermediateNode calculates an interior node's value by H(left || right)
-func HashIntermediateNode(left []byte, right []byte) []byte {
-	h := sha256.New()
-	h.Write(left)
-	h.Write(right)
-	return h.Sum(nil)
-}
-
-// EmptyLeafValue computes the value of an empty leaf as
-// H(TreeNonce || EmptyIdentifier || depth || index), where TreeNonce,
-// EmptyIdentifier, depth, and index are fixed-length.
-func EmptyLeafValue(prefix string) []byte {
-	return HashLeaf(EmptyIdentifier, len(prefix), []byte(prefix), nil)
 }
 
 // Hash calculates the hash of the given data.
