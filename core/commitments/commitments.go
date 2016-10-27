7e954b03eb43985380d6b00e8b260290e6594a25
Gary Belvin <gdb@google.com>
2016-10-27 09:58:44 -0700

    Fix Commitment Scheme
    Per security review recommendations, adjust the commitment scheme from
    ` HMAC(nonce, message) `
    to
    ` HMAC(fixedKey, r || message)`

--- core/commitments/commitments.go
+++ core/commitments/commitments.go
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package commitments contains common type definitions and functions used by other
-// packages. Types that can cause circular import should be added here.
+// Package commitments implements a cryptographic commitment.
+//
+// Commitment scheme is as follows:
+// T = HMAC(fixedKey, "Key Transparency Commitment" || 16 byte nonce || message)
+// message is defined as: len(userID) || userID || data
 package commitments
 
 import (
@@ -32,10 +35,15 @@ const (
 	// commitmentKeyLen should be robust against the birthday attack.
 	// One commitment is given for each leaf node throughout time.
 	commitmentKeyLen = 16 // 128 bits of security, supports 2^64 nodes.
+	// prefix is a string used to make the commitments from this package unique.
+	prefix = "Key Transparency Commitment"
 )
 
 var (
 	hashAlgo = sha512.New512_256
+	// key is publicly known random fixed key for use in the HMAC function.
+	// This fixed key allows the commitment scheme to be modeled as a random oracle.
+	fixedKey = []byte{0x19, 0x6e, 0x7e, 0x52, 0x84, 0xa7, 0xef, 0x93, 0x0e, 0xcb, 0x9a, 0x19, 0x78, 0x74, 0x97, 0x55}
 	// ErrInvalidCommitment occurs when the commitment doesn't match the profile.
 	ErrInvalidCommitment = errors.New("invalid commitment")
 )
@@ -50,29 +58,39 @@ type Committer interface {
 
 // Commit makes a cryptographic commitment under a specific userID to data.
 func Commit(userID string, data []byte) ([]byte, *tpb.Committed, error) {
-	// Generate commitment key.
-	key := make([]byte, commitmentKeyLen)
-	if _, err := rand.Read(key); err != nil {
+	// Generate commitment nonce.
+	nonce := make([]byte, commitmentKeyLen)
+	if _, err := rand.Read(nonce); err != nil {
 		return nil, nil, err
 	}
 
-	mac := hmac.New(hashAlgo, key)
+	mac := hmac.New(hashAlgo, fixedKey)
+	mac.Write([]byte(prefix))
+	mac.Write(nonce)
+
+	// Message
 	strlen := make([]byte, 4)
 	binary.BigEndian.PutUint32(strlen, uint32(len(userID)))
 	mac.Write(strlen)
 	mac.Write([]byte(userID))
 	mac.Write(data)
-	return mac.Sum(nil), &tpb.Committed{Key: key, Data: data}, nil
+
+	return mac.Sum(nil), &tpb.Committed{Key: nonce, Data: data}, nil
 }
 
 // Verify customizes a commitment with a userID.
 func Verify(userID string, commitment []byte, committed *tpb.Committed) error {
-	mac := hmac.New(hashAlgo, committed.Key)
+	mac := hmac.New(hashAlgo, fixedKey)
+	mac.Write([]byte(prefix))
+	mac.Write(committed.Key)
+
+	// Message
 	strlen := make([]byte, 4)
 	binary.BigEndian.PutUint32(strlen, uint32(len(userID)))
 	mac.Write(strlen)
 	mac.Write([]byte(userID))
 	mac.Write(committed.Data)
+
 	if !hmac.Equal(mac.Sum(nil), commitment) {
 		return ErrInvalidCommitment
 	}
