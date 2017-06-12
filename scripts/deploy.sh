51df996ae2dbc884d8d2106c37410a490d8ac0ab
Ismail Khoffi <ismailkhoffi@google.com>
2017-06-12 18:38:30 +0100

    Work in progress: add encrypted credentials (and vars to travis) - update gitignore - not yet working kubernetes db deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME=transparent-keyserver
-#TODO(ismail): build with docker-compose, then (re)tag and push
 # build using docker compose
 docker-compose build
 
@@ -11,7 +10,15 @@ images=("trillian_log_server" "trillian_map_server" "keytransparency-server" \
 "trillian_log_signer" "keytransparency-signer")
 for DOCKER_IMAGE_NAME in "${images[@]}"
 do
-  echo ${DOCKER_IMAGE_NAME}
-	docker tag ${DOCKER_IMAGE_NAME} us.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
-	#TODO gcloud docker -- push us.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}
+  # TODO(ismail): do we want to update the images every commit? Probably not!
+  # TODO(ismail): figure out a good workflow, e.g. deploy images for PRs to master
+  # then, run some integration tests (already in install and not in deploy).
+  # If we are a commit on master: do the same as above but tag images differently.
+	docker tag ${DOCKER_IMAGE_NAME} us.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}
+	gcloud docker -- push us.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}
 done
+
+# TODO(ismail): actually run the images and some integration tests
+# TODO(ismail): additionally to above container images we might want one that
+# simply queries (the equivalent of) https://localhost:8080/v1/users/foo@bar.com
+# later we would want one that runs a client, too
