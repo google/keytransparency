9ba3a83202430b3335898f8fdb0e090aba2fe437
Ismail Khoffi <ismailkhoffi@google.com>
2017-06-13 14:43:46 +0100

    only run deploy script if we are not in a PR but a master commit only decrypt the files after travis succeeds and we are merging to master travis will fail on forks trying to decrypt anyways

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME=transparent-keyserver
-# build using docker compose
-docker-compose build
 
-#TODO(ismail): this needs to be manually synced with docker-compose.yml; find a
-# better way
-images=("trillian_log_server" "trillian_map_server" "keytransparency-server" \
-"trillian_log_signer" "keytransparency-signer")
-for DOCKER_IMAGE_NAME in "${images[@]}"
-do
-  # TODO(ismail): do we want to update the images every commit? Probably not!
-  # TODO(ismail): figure out a good workflow, e.g. deploy images for PRs to master
-  # then, run some integration tests (already in install and not in deploy).
-  # If we are a commit on master: do the same as above but tag images differently.
-	docker tag ${DOCKER_IMAGE_NAME} us.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}
-	gcloud docker -- push us.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}
-done
+# Make sure we are in a travis build environment:
+if [ ! "$TRAVIS" = "true" ] && [ ! "$CI" = "true" ]; then
+  echo "$0 is only meant to run inside travis images/from travis.yml"
+  exit 1
+fi
 
-# TODO(ismail): actually run the images and some integration tests
-# TODO(ismail): additionally to above container images we might want one that
-# simply queries (the equivalent of) https://localhost:8080/v1/users/foo@bar.com
-# later we would want one that runs a client, too
+# Only run deploy if we are on master and not a PR:
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  gcloud --quiet version
+  gcloud auth activate-service-account --key-file client-secret.json
+  gcloud --quiet components update kubectl
+  gcloud config set project ${PROJECT_NAME}
+  gcloud config set compute/zone us-central1-b
+
+  # Build using docker compose
+  docker-compose build
+
+  #TODO(ismail): this needs to be manually synced with docker-compose.yml; find a
+  # better way
+  images=("trillian_log_server" "trillian_map_server" "keytransparency-server" \
+  "trillian_log_signer" "keytransparency-signer")
+  for DOCKER_IMAGE_NAME in "${images[@]}"
+  do
+    # Tag with current commit: If sth fails we can reproduce which merge
+    # triggered the error:
+    docker tag ${DOCKER_IMAGE_NAME} us.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}
+    # Push the images as we refer to them in the kubernetes config files:
+    gcloud docker -- push us.gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}
+  done
+
+  # TODO(ismail): actually run the images and some integration tests
+  # kubectl apply -f kubernetes/keytransparency-deployment.yml
+  # TODO(ismail): additionally to above container images we might want one that
+  # simply queries (the equivalent of) https://localhost:8080/v1/users/foo@bar.com
+  # later we would want one that runs a client, too
+fi
