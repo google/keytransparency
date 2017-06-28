f32654b988e6a8ca83293858972f03d2e0aa3a10
juniway <juniway@sina.com>
2017-06-28 23:41:31 +0800

    fix password length error, compatible issue with openssl 1.1.0(#658)

--- scripts/gen_server_keys.sh
+++ scripts/gen_server_keys.sh
@@ -40,8 +40,8 @@ mkdir -p "${GOPATH}/src/github.com/google/keytransparency/genfiles"
 cd "${GOPATH}/src/github.com/google/keytransparency/genfiles"
 
 # Generate TLS keys.
-openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-openssl rsa -passin pass:x -in server.pass.key -out server.key
+openssl genrsa -des3 -passout pass:xxxx -out server.pass.key 2048
+openssl rsa -passin pass:xxxx -in server.pass.key -out server.key
 chmod 600 server.key
 rm server.pass.key
 openssl req -new \
