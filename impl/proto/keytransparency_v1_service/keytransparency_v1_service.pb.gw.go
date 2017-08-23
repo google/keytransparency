57a901676b86683427181d51f071a5569e9b19a3
Gary Belvin <gdb@google.com>
2017-08-23 14:40:26 -0400

    BatchUpdateEntries

--- impl/proto/keytransparency_v1_service/keytransparency_v1_service.pb.gw.go
+++ impl/proto/keytransparency_v1_service/keytransparency_v1_service.pb.gw.go
@@ -147,18 +147,18 @@ func request_KeyTransparencyService_GetDomainInfo_0(ctx context.Context, marshal
 }
 
 var (
-	filter_KeyTransparencyAdminService_BatchSetEntries_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+	filter_KeyTransparencyAdminService_BatchUpdateEntries_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
-func request_KeyTransparencyAdminService_BatchSetEntries_0(ctx context.Context, marshaler runtime.Marshaler, client KeyTransparencyAdminServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq keytransparency_v1_types.BatchSetEntriesRequest
+func request_KeyTransparencyAdminService_BatchUpdateEntries_0(ctx context.Context, marshaler runtime.Marshaler, client KeyTransparencyAdminServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq keytransparency_v1_types.BatchUpdateEntriesRequest
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_KeyTransparencyAdminService_BatchSetEntries_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_KeyTransparencyAdminService_BatchUpdateEntries_0); err != nil {
 		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.BatchSetEntries(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.BatchUpdateEntries(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
@@ -358,7 +358,7 @@ func RegisterKeyTransparencyAdminServiceHandlerFromEndpoint(ctx context.Context,
 func RegisterKeyTransparencyAdminServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
 	client := NewKeyTransparencyAdminServiceClient(conn)
 
-	mux.Handle("POST", pattern_KeyTransparencyAdminService_BatchSetEntries_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_KeyTransparencyAdminService_BatchUpdateEntries_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(ctx)
 		defer cancel()
 		if cn, ok := w.(http.CloseNotifier); ok {
@@ -375,14 +375,14 @@ func RegisterKeyTransparencyAdminServiceHandler(ctx context.Context, mux *runtim
 		if err != nil {
 			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
 		}
-		resp, md, err := request_KeyTransparencyAdminService_BatchSetEntries_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_KeyTransparencyAdminService_BatchUpdateEntries_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_KeyTransparencyAdminService_BatchSetEntries_0(ctx, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_KeyTransparencyAdminService_BatchUpdateEntries_0(ctx, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -390,9 +390,9 @@ func RegisterKeyTransparencyAdminServiceHandler(ctx context.Context, mux *runtim
 }
 
 var (
-	pattern_KeyTransparencyAdminService_BatchSetEntries_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "users"}, "batch"))
+	pattern_KeyTransparencyAdminService_BatchUpdateEntries_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "users"}, "batch"))
 )
 
 var (
-	forward_KeyTransparencyAdminService_BatchSetEntries_0 = runtime.ForwardResponseMessage
+	forward_KeyTransparencyAdminService_BatchUpdateEntries_0 = runtime.ForwardResponseMessage
 )
