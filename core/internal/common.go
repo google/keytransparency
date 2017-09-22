1a74eb874f820a3c3ea41357301bc1b4175ab07d
Paul Hadfield <hadfieldp@google.com>
2017-09-22 16:01:11 +0100

    Use Trillian Map's new way of storing MapperMetadata.
    See https://github.com/google/trillian/pull/838
    
    Trillian has withdrawn the MapperMetadata protocol message in favor of
    using google.protobuf.Any instead.  Map personalities can store any
    protocol message they want in Signed Map Head metadata but must
    marshal/unmarshal to Any when doing so.
    
    This commit does the marshalling / unmarshalling in Key Transparency.
    
    See:
    - https://github.com/google/trillian/pull/838
    - https://github.com/google/trillian/commit/80d7516

---
+++ core/internal/common.go
@@ -0,0 +1,52 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"fmt"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/any"
+
+	tpb "github.com/google/keytransparency/core/proto/keytransparency_v1_types"
+	"github.com/google/trillian"
+)
+
+// MetadataFromMapRoot converts the google.protobuf.Any metadata field from the
+// Trillian SignedMapRoot into a tpb.MapperMetadata, or returns an error if the
+// field is absent or of the wrong type.
+func MetadataFromMapRoot(r *trillian.SignedMapRoot) (*tpb.MapperMetadata, error) {
+	if r.GetMetadata() == nil {
+		return &tpb.MapperMetadata{}, nil
+	}
+	var metadataProto ptypes.DynamicAny
+	if err := ptypes.UnmarshalAny(r.GetMetadata(), &metadataProto); err != nil {
+		return nil, fmt.Errorf("Failed to unmarshal MapRoot.Metadata: %v", err)
+	}
+	return metadataProto.Message.(*tpb.MapperMetadata), nil
+}
+
+// MetadataAsAny marshals the supplied MapperMetadata proto into an 'any', which
+// can be supplied as metadata to the Trillian map when setting leaves.
+func MetadataAsAny(meta *tpb.MapperMetadata) (*any.Any, error) {
+	if meta == nil {
+		return &any.Any{}, nil
+	}
+	metaAny, err := ptypes.MarshalAny(meta)
+	if err != nil {
+		return nil, err
+	}
+	return metaAny, nil
+}
