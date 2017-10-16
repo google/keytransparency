0e85d410a0f9e91d9e19fe865235aff8e9a40a05
Gary Belvin <gdb@google.com>
2017-10-16 10:09:44 +0100

    package comment

--- core/monitor/storage/storage.go
+++ core/monitor/storage/storage.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package storage holds data produced by the monitor
 package storage
 
 import (
