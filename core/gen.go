e8cf72b0f808e8c398457c3ebfd196f10fb8115a
Gary Belvin <gdb@google.com>
2017-10-19 10:06:16 +0100

    Generate protos from a common directory
    protoc uses the path passed to protoc as the prefix for symbol
    resolution. If protoc is invoked from different directories, the
    resulting FileDescriptors will have different symbols, either causing
    the symbols to duplicate, or be unresolvable.
    
    The solution in this commit is to generate all the protos for this
    project from the same directory like we did for Trillian.
    
    This also has the side benefit of producing a single directory for
    reverse symbol lookkup when using grpc reflection with the grpc_cli
    tool.

---
+++ core/gen.go
@@ -0,0 +1,32 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package core
+
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis/ --go_out=:$GOPATH/src proto/keytransparency_v1_proto/keytransparency_v1_proto.proto
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis/ --go_out=,plugins=grpc:$GOPATH/src proto/keytransparency_v1_grpc/keytransparency_v1_grpc.proto
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis/ --grpc-gateway_out=logtostderr=true:. proto/keytransparency_v1_grpc/keytransparency_v1_grpc.proto
+
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis/ --go_out=,plugins=grpc:$GOPATH/src proto/sequencer_v1_grpc/sequencer_v1_grpc.proto
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis/ --grpc-gateway_out=logtostderr=true:. proto/sequencer_v1_grpc/sequencer_v1_grpc.proto
+
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis --go_out=:$GOPATH/src proto/monitor_v1_proto/monitor_v1_proto.proto
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis/ --go_out=,plugins=grpc:$GOPATH/src proto/monitor_v1_grpc/monitor_v1_grpc.proto
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis/ --grpc-gateway_out=logtostderr=true:. proto/monitor_v1_grpc/monitor_v1_grpc.proto
+
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis/ --go_out=,plugins=grpc:$GOPATH/src proto/mutation_v1_grpc/mutation_v1_grpc.proto
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/google/trillian/ -I=$GOPATH/src/github.com/googleapis/googleapis/ --grpc-gateway_out=logtostderr=true:. proto/mutation_v1_grpc/mutation_v1_grpc.proto
+
+//go:generate protoc -I=. --go_out=:$GOPATH/src proto/authorization_proto/authorization_proto.proto
+//go:generate protoc -I=. --go_out=:$GOPATH/src proto/keymaster_proto/keymaster_proto.proto
