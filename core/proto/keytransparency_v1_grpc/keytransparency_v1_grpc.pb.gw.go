67d39b7091c15ce5c897b66e7b69f218286796aa
Gary Belvin <gdbelvin@gmail.com>
2017-10-23 15:19:01 +0100

    New Admin API for managing domains (#843)
    * New Admin API for managing domains
    
    * Add ShowDeleted to GetDomainsRequest
    
    * Add docs
    
    * Spelling nit

--- core/proto/keytransparency_v1_grpc/keytransparency_v1_grpc.pb.gw.go
+++ core/proto/keytransparency_v1_grpc/keytransparency_v1_grpc.pb.gw.go
@@ -146,23 +146,6 @@ func request_KeyTransparencyService_GetDomainInfo_0(ctx context.Context, marshal
 
 }
 
-var (
-	filter_KeyTransparencyAdminService_BatchUpdateEntries_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
-func request_KeyTransparencyAdminService_BatchUpdateEntries_0(ctx context.Context, marshaler runtime.Marshaler, client KeyTransparencyAdminServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq keytransparency_v1_proto.BatchUpdateEntriesRequest
-	var metadata runtime.ServerMetadata
-
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_KeyTransparencyAdminService_BatchUpdateEntries_0); err != nil {
-		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.BatchUpdateEntries(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
 // RegisterKeyTransparencyServiceHandlerFromEndpoint is same as RegisterKeyTransparencyServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterKeyTransparencyServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -326,73 +309,4 @@ var (
 	forward_KeyTransparencyService_UpdateEntry_0 = runtime.ForwardResponseMessage
 
 	forward_KeyTransparencyService_GetDomainInfo_0 = runtime.ForwardResponseMessage
-)
-
-// RegisterKeyTransparencyAdminServiceHandlerFromEndpoint is same as RegisterKeyTransparencyAdminServiceHandler but
-// automatically dials to "endpoint" and closes the connection when "ctx" gets done.
-func RegisterKeyTransparencyAdminServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err != nil {
-			if cerr := conn.Close(); cerr != nil {
-				grpclog.Printf("Failed to close conn to %s: %v", endpoint, cerr)
-			}
-			return
-		}
-		go func() {
-			<-ctx.Done()
-			if cerr := conn.Close(); cerr != nil {
-				grpclog.Printf("Failed to close conn to %s: %v", endpoint, cerr)
-			}
-		}()
-	}()
-
-	return RegisterKeyTransparencyAdminServiceHandler(ctx, mux, conn)
-}
-
-// RegisterKeyTransparencyAdminServiceHandler registers the http handlers for service KeyTransparencyAdminService to "mux".
-// The handlers forward requests to the grpc endpoint over "conn".
-func RegisterKeyTransparencyAdminServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
-	client := NewKeyTransparencyAdminServiceClient(conn)
-
-	mux.Handle("POST", pattern_KeyTransparencyAdminService_BatchUpdateEntries_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(ctx)
-		defer cancel()
-		if cn, ok := w.(http.CloseNotifier); ok {
-			go func(done <-chan struct{}, closed <-chan bool) {
-				select {
-				case <-done:
-				case <-closed:
-					cancel()
-				}
-			}(ctx.Done(), cn.CloseNotify())
-		}
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, req)
-		if err != nil {
-			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
-		}
-		resp, md, err := request_KeyTransparencyAdminService_BatchUpdateEntries_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_KeyTransparencyAdminService_BatchUpdateEntries_0(ctx, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	return nil
-}
-
-var (
-	pattern_KeyTransparencyAdminService_BatchUpdateEntries_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "users"}, "batch"))
-)
-
-var (
-	forward_KeyTransparencyAdminService_BatchUpdateEntries_0 = runtime.ForwardResponseMessage
 )
