8f8870da91e84ef91c3b2339097a7fa0dfa8f3fa
Gary Belvin <gdb@google.com>
2017-11-15 14:44:19 +0000

    Add URL for available domains

--- core/proto/keytransparency_v1_grpc/keytransparency_v1_admin_grpc.pb.gw.go
+++ core/proto/keytransparency_v1_grpc/keytransparency_v1_admin_grpc.pb.gw.go
@@ -46,6 +46,23 @@ func request_KeyTransparencyAdminService_BatchUpdateEntries_0(ctx context.Contex
 }
 
 var (
+	filter_KeyTransparencyAdminService_ListDomains_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_KeyTransparencyAdminService_ListDomains_0(ctx context.Context, marshaler runtime.Marshaler, client KeyTransparencyAdminServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq keytransparency_v1_proto.ListDomainsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_KeyTransparencyAdminService_ListDomains_0); err != nil {
+		return nil, metadata, grpc.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.ListDomains(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+var (
 	filter_KeyTransparencyAdminService_GetDomain_0 = &utilities.DoubleArray{Encoding: map[string]int{"domain_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
 
@@ -205,6 +222,34 @@ func RegisterKeyTransparencyAdminServiceHandler(ctx context.Context, mux *runtim
 
 	})
 
+	mux.Handle("GET", pattern_KeyTransparencyAdminService_ListDomains_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, req)
+		if err != nil {
+			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
+		}
+		resp, md, err := request_KeyTransparencyAdminService_ListDomains_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_KeyTransparencyAdminService_ListDomains_0(ctx, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_KeyTransparencyAdminService_GetDomain_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(ctx)
 		defer cancel()
@@ -323,6 +368,8 @@ func RegisterKeyTransparencyAdminServiceHandler(ctx context.Context, mux *runtim
 var (
 	pattern_KeyTransparencyAdminService_BatchUpdateEntries_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "users"}, "batch"))
 
+	pattern_KeyTransparencyAdminService_ListDomains_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "domains"}, ""))
+
 	pattern_KeyTransparencyAdminService_GetDomain_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "domains", "domain_id"}, ""))
 
 	pattern_KeyTransparencyAdminService_CreateDomain_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "domains"}, ""))
@@ -334,6 +381,8 @@ var (
 
 var (
 	forward_KeyTransparencyAdminService_BatchUpdateEntries_0 = runtime.ForwardResponseMessage
+
+	forward_KeyTransparencyAdminService_ListDomains_0 = runtime.ForwardResponseMessage
 
 	forward_KeyTransparencyAdminService_GetDomain_0 = runtime.ForwardResponseMessage
 
