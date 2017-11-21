9c0af88675a196c699efb6ed67169f9fbf403464
Gary Belvin <gdb@google.com>
2017-11-21 16:16:03 +0000

    Update docker-compose
    This updates docker-compose so that it supports a single-line cluster
    bring up.
    - docker-compose up -d
    - Auto initializes with an init container
    - Changed service names to have `-` rather than `_` to be kubernetes
    compatable.
    - docker-compose is now fully kompose compatible.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,105 +17,19 @@
 ################################################################################
 
 set -ex
-PROJECT_NAME=key-transparency
-
-MAX_RETRY=30
-
-dir="$(dirname "$0")"
-# Import createTree commands:
-source ${dir}/configure_trillian.sh
 
 function main()
 {
-  checkCmdsAvailable
   # create key-pairs:
   ./scripts/prepare_server.sh -f
-  prepareSecrets
-  buildDockerImgs
-
-  # Deploy all trillian related services.
-  # the following line only restarts the DB if its config changed:
-  kubectl apply -f deploy/kubernetes/db-deployment.yml
-  kubectl apply -f deploy/kubernetes/trillian-deployment.yml
-  # Deploy all keytransparency related services (server and signer):
-  kubectl apply -f deploy/kubernetes/keytransparency-deployment.yml
-
-  waitForTrillian
-  createTreeIfNeccessaryAndSetIDs
-}
-
-
-function buildDockerImgs()
-{
-  # Work around some git permission issues on linux:
-  chmod a+r ../trillian/storage/mysql/storage.sql
-
-  # Build all images defined in the docker-compose.yml:
+  #prepareSecrets # TODO(gbelvin): Use secrets volume.
   docker-compose build
   docker-compose push
+
+  # Deploy all trillian related services.
+  kubectl apply -f deploy/kubernetes/.
 }
 
-function waitForTrillian()
-{
-  # It's very unlikely that everything is up running before 15 sec.:
-  sleep 15
-  # Wait for trillian-map pod to be up:
-  COUNTER=0
-  MAPSRV=""
-  until [ -n "$MAPSRV" ] || [  $COUNTER -gt $MAX_RETRY ]; do
-    # Service wasn't up yet:
-    sleep 10;
-    let COUNTER+=1
-    MAPSRV=$(kubectl get pods --selector=run=map_server -o jsonpath={.items[*].metadata.name});
-  done
-
-  if [ -n "$MAPSRV" ]; then
-    echo "trillian-map service is up"
-  else
-    echo "Stopped waiting for trillian-map service. Quitting ..."
-    exit 1;
-  fi
-}
-
-function createTreeIfNeccessaryAndSetIDs()
-{
-  LOG_ID=""
-  MAP_ID=""
-  export LOCAL=false;
-
-  # first request domain info:
-  retrieveTrees
-
-  if [ -n "$LOG_ID" ] && [ -n "$MAP_ID" ]; then
-    echo "Using existing trees with MAP_ID=$MAP_ID and LOG_ID=$LOG_ID"
-  fi
-
-  COUNTER=0
-  until [ -n "$LOG_ID" ] || [  $COUNTER -gt $MAX_RETRY ]; do
-    # RPC was not available yet, wait and retry:
-    sleep 10;
-    let COUNTER+=1;
-    createLog && createMap
-  done
-
-  if [ -n "$LOG_ID" ] && [ -n "$MAP_ID" ]; then
-    echo "Trees created with MAP_ID=$MAP_ID and LOG_ID=$LOG_ID"
-    # Substitute LOG_ID and MAP_ID in template kubernetes file:
-    sed 's/${LOG_ID}'/${LOG_ID}/g deploy/kubernetes/keytransparency-deployment.yml.tmpl > deploy/kubernetes/keytransparency-deployment.yml
-    sed -i 's/${MAP_ID}'/${MAP_ID}/g deploy/kubernetes/keytransparency-deployment.yml
-  else
-    echo "Failed to create tree. Need map-id and log-id before running kt-server/-signer."
-    exit 1
-  fi
-}
-
-function checkCmdsAvailable()
-{
-  if ! type jq > /dev/null 2>&1;
-    then echo "Please install jq. See: https://stedolan.github.io/jq/download/"
-    exit 1
-  fi
-}
 
 function prepareSecrets()
 {
