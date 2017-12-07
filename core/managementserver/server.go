3ed081a43234e6e72b45dfb5980be16b42b7e408
Gary Belvin <gdb@google.com>
2017-12-07 14:33:58 +0000

    Reviewer nits

--- core/managementserver/server.go
+++ core/managementserver/server.go
@@ -18,6 +18,9 @@ package managementserver
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	pb "github.com/google/keytransparency/core/api/usermanager/v1/usermanager_proto"
 	keypb "github.com/google/keytransparency/core/proto/keymaster_proto"
 	ktpb "github.com/google/keytransparency/core/proto/keytransparency_v1_proto"
@@ -34,26 +37,26 @@ func New() *Server {
 // GetKeySet returns a list of public keys (a keyset) that corresponds to the signing keys
 // this service has for a given domain and app.
 func (s *Server) GetKeySet(context.Context, *pb.GetKeySetRequest) (*keypb.KeySet, error) {
-	panic("unimplemented")
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
 }
 
 // CreateUser creates a new user and initializes it.
 // If the user already exists, this operation will fail.
 func (s *Server) CreateUser(context.Context, *pb.CreateUserRequest) (*ktpb.User, error) {
-	panic("unimplemented")
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
 }
 
 // UpdateUser sets the public key for an user.
 func (s *Server) UpdateUser(context.Context, *pb.UpdateUserRequest) (*ktpb.User, error) {
-	panic("unimplemented")
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
 }
 
 // BatchCreateUser creates a set of new users.
 func (s *Server) BatchCreateUser(context.Context, *pb.BatchCreateUserRequest) (*ktpb.User, error) {
-	panic("unimplemented")
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
 }
 
 // BatchUpdateUser updates a set of users.
 func (s *Server) BatchUpdateUser(context.Context, *pb.BatchUpdateUserRequest) (*ktpb.User, error) {
-	panic("unimplemented")
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
 }
