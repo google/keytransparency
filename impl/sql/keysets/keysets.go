346608258b8d85c4f94f0f08aefaf0b20d427a85
Gary Belvin <gdb@google.com>
2017-12-21 17:13:35 +0000

    sql keyset implementation

---
+++ impl/sql/keysets/keysets.go
@@ -0,0 +1,128 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package keysets implements the storage.KeySets interface.
+package keysets
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/google/keytransparency/core/storage"
+
+	"github.com/golang/protobuf/proto"
+
+	tpb "github.com/google/keytransparency/core/api/type/type_proto"
+)
+
+const (
+	schema = `
+CREATE TABLE IF NOT EXISTS KeySets(
+	InstanceID            BIGINT NOT NULL,
+        DomainID              VARCHAR(40) NOT NULL,
+        AppID                 VARCHAR(40) NOT NULL,
+        KeySet                MEDIUMBLOB NOT NULL,
+        PRIMARY KEY(InstanceID,DomainID,AppID)
+);`
+
+	getSQL = `SELECT * FROM KeySets WHERE InstanceID = ? AND DomainID = ? AND AppID = ?`
+	setSQL = `INSERT INTO KeySets (InstanceID, DomainID, AppID, KeySet) VALUES (?, ?, ?, ?);`
+)
+
+// Storage stores keysets.
+type Storage struct {
+	db *sql.DB
+}
+
+type keyset struct {
+	InstanceID int64
+	DomainID   string
+	AppID      string
+	KeySet     []byte
+}
+
+// newkeyset converts a tpb.KeySet to keyset.
+func newkeyset(instance int64, domainID, appID string, k *tpb.KeySet) (*keyset, error) {
+	data, err := proto.Marshal(k)
+	if err != nil {
+		return nil, err
+	}
+	return &keyset{
+		InstanceID: instance,
+		DomainID:   domainID,
+		AppID:      appID,
+		KeySet:     data,
+	}, nil
+}
+
+// Proto converts a keyset to a tpb.KeySet proto.
+func (k *keyset) Proto() (*tpb.KeySet, error) {
+	ks := tpb.KeySet{}
+	if err := proto.Unmarshal(k.KeySet, &ks); err != nil {
+		return nil, err
+	}
+	return &ks, nil
+}
+
+// New returns a storage.KeySets client backed by an SQL table.
+func New(db *sql.DB) (storage.KeySets, error) {
+	s := &Storage{
+		db: db,
+	}
+	// Create schema.
+	if _, err := s.db.Exec(schema); err != nil {
+		return nil, fmt.Errorf("failed to create keyset table: %v", err)
+	}
+	return s, db.Ping()
+}
+
+// Get returns a stored keyset.
+func (s *Storage) Get(ctx context.Context, instance int64, domainID, appID string) (*tpb.KeySet, error) {
+	readStmt, err := s.db.PrepareContext(ctx, getSQL)
+	if err != nil {
+		return nil, err
+	}
+	defer readStmt.Close()
+	r := keyset{}
+	if err := readStmt.QueryRowContext(ctx, instance, domainID, appID).Scan(
+		&r.InstanceID,
+		&r.DomainID,
+		&r.AppID,
+		&r.KeySet); err != nil {
+		return nil, err
+	}
+	return r.Proto()
+}
+
+// Set saves a keyset.
+func (s *Storage) Set(ctx context.Context, instance int64, domainID, appID string, k *tpb.KeySet) error {
+	r, err := newkeyset(instance, domainID, appID, k)
+	if err != nil {
+		return err
+	}
+
+	// Prepare SQL.
+	writeStmt, err := s.db.PrepareContext(ctx, setSQL)
+	if err != nil {
+		return err
+	}
+	defer writeStmt.Close()
+	_, err = writeStmt.ExecContext(ctx,
+		r.InstanceID,
+		r.DomainID,
+		r.AppID,
+		r.KeySet)
+	return err
+}
---
+++ impl/sql/keysets/keysets_test.go
@@ -0,0 +1,78 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package keysets
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/google/go-cmp/cmp"
+
+	tpb "github.com/google/keytransparency/core/api/type/type_proto"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func TestWriteRead(t *testing.T) {
+	ctx := context.Background()
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatalf("sql.Open(): %v", err)
+	}
+	defer db.Close()
+	keysets, err := New(db)
+	if err != nil {
+		t.Fatalf("Failed to create keysets.Storage")
+	}
+
+	for _, tc := range []struct {
+		instanceID int64
+		domainID   string
+		appID      string
+		ks         *tpb.KeySet
+		write      bool
+		writeErr   bool
+		read       bool
+		readErr    bool
+	}{
+		{
+			instanceID: 0,
+			domainID:   "domain",
+			appID:      "app",
+			ks: &tpb.KeySet{VerifyingKeys: map[string]*tpb.VerifyingKey{
+				"1": &tpb.VerifyingKey{KeyMaterial: []byte("keydata")},
+			}},
+			write: true,
+			read:  true,
+		},
+	} {
+		if tc.write {
+			err := keysets.Set(ctx, tc.instanceID, tc.domainID, tc.appID, tc.ks)
+			if got, want := err != nil, tc.writeErr; got != want {
+				t.Errorf("Set(%v): %v, wantErr %v", tc.instanceID, err, want)
+			}
+		}
+		if tc.read {
+			ks, err := keysets.Get(ctx, tc.instanceID, tc.domainID, tc.appID)
+			if got, want := err != nil, tc.readErr; got != want {
+				t.Errorf("Read(%v): %v, wantErr %v", tc.instanceID, err, want)
+			}
+			if got, want := ks, tc.ks; !cmp.Equal(got, want, cmp.Comparer(proto.Equal)) {
+				t.Errorf("Read(%v): %v, want %v", tc.instanceID, got, want)
+			}
+		}
+	}
+}
