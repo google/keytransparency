df5f2fdcffb8bd6eb4a2ad583be3a0bceaae521e
Gary Belvin <gdbelvin@gmail.com>
2018-02-02 13:23:19 +0000

    Consistency nits (#922)
    * Use same order of arguments for sequencer and keyserver
    
    * Add package comment

--- core/integration/alltests.go
+++ core/integration/alltests.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package integration exports a set of unit tests that can be run by impl/integration
+// or any other specific instantiation of KeyTransparency.
 package integration
 
 import (
