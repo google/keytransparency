2db33d4c731702f5b1d06d65402b4390603e47aa
Gary Belvin <gdb@google.com>
2018-05-16 13:17:55 +0100

    Onlu send user creds for Update

--- cmd/keytransparency-client/cmd/history.go
+++ cmd/keytransparency-client/cmd/history.go
@@ -47,11 +47,7 @@ and verify that the results are consistent.`,
 		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
 
-		userCreds, err := userCreds(ctx, false)
-		if err != nil {
-			return err
-		}
-		c, err := GetClient(ctx, userCreds)
+		c, err := GetClient(ctx)
 		if err != nil {
 			return fmt.Errorf("Error connecting: %v", err)
 		}
