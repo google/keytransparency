2fa48d9540c5ec039aedaed6265bb9d3debf3ae3
Gary Belvin <gdb@google.com>
2018-06-20 15:55:08 +0100

    Use glog, not log

--- cmd/keytransparency-sequencer/server.go
+++ cmd/keytransparency-sequencer/server.go
@@ -16,7 +16,6 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net/http"
 
 	"github.com/google/keytransparency/cmd/serverutil"
@@ -67,9 +66,9 @@ func startHTTPServer(svr pb.KeyTransparencyAdminServer) *http.Server {
 	metricMux := http.NewServeMux()
 	metricMux.Handle("/metrics", promhttp.Handler())
 	go func() {
-		log.Printf("Hosting metrics on %v", *metricsAddr)
+		glog.Infof("Hosting metrics on %v", *metricsAddr)
 		if err := http.ListenAndServe(*metricsAddr, metricMux); err != nil {
-			log.Fatalf("ListenAndServeTLS(%v): %v", *metricsAddr, err)
+			glog.Fatalf("ListenAndServeTLS(%v): %v", *metricsAddr, err)
 		}
 	}()
 
