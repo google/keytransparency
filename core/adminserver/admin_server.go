684bc23d435b09c5a7b92b24d5dc12909092b5bf
Gary Belvin <gdb@google.com>
2018-07-10 21:53:24 +0100

    Explicitly trust the first observed log root
    google/trillian#1224 requires clients to explicity state what trusted
    root to initially use when verifying responses from the trillian log.
    
    This PR uses an empty root, effectively trusting the first observed
    log root.

--- core/adminserver/admin_server.go
+++ core/adminserver/admin_server.go
@@ -278,9 +278,9 @@ func (s *Server) CreateDomain(ctx context.Context, in *pb.CreateDomainRequest) (
 func (s *Server) initialize(ctx context.Context, logTree, mapTree *tpb.Tree) error {
 	logID := logTree.GetTreeId()
 	mapID := mapTree.GetTreeId()
+
 	// TODO(gbelvin): Store and track trusted root.
 	trustedRoot := types.LogRootV1{} // Automatically trust the first observed log root.
-
 	logClient, err := client.NewFromTree(s.tlog, logTree, trustedRoot)
 	if err != nil {
 		return fmt.Errorf("adminserver: could not create log client: %v", err)
