37102fa4dad9beb741ee75e476858414eef0dc2e
Gary Belvin <gdbelvin@gmail.com>
2018-07-17 16:14:15 +0100

    Cleanup domains after integration test (#1019)
    * Cleanup after integration tests
    
    * Delete trillian trees
    
    * Add tests
    
    * Add test for list domains
    
    * Remove undelete from this PR

--- core/adminserver/admin_server.go
+++ core/adminserver/admin_server.go
@@ -325,16 +325,26 @@ func (s *Server) initialize(ctx context.Context, logTree, mapTree *tpb.Tree) err
 
 // DeleteDomain marks a domain as deleted, but does not immediately delete it.
 func (s *Server) DeleteDomain(ctx context.Context, in *pb.DeleteDomainRequest) (*google_protobuf.Empty, error) {
+	d, err := s.GetDomain(ctx, &pb.GetDomainRequest{DomainId: in.GetDomainId()})
+	if err != nil {
+		return nil, err
+	}
+
 	if err := s.domains.SetDelete(ctx, in.GetDomainId(), true); err != nil {
 		return nil, err
 	}
+
+	_, delLogErr := s.logAdmin.DeleteTree(ctx, &tpb.DeleteTreeRequest{TreeId: d.Log.TreeId})
+	_, delMapErr := s.mapAdmin.DeleteTree(ctx, &tpb.DeleteTreeRequest{TreeId: d.Map.TreeId})
+	if delLogErr != nil || delMapErr != nil {
+		return nil, status.Errorf(codes.Internal, "adminserver: delete log %v: %v, delete map %v: %v",
+			err, d.Log.TreeId, delLogErr, d.Map.TreeId, delMapErr)
+	}
+
 	return &google_protobuf.Empty{}, nil
 }
 
 // UndeleteDomain reactivates a deleted domain - provided that UndeleteDomain is called sufficiently soon after DeleteDomain.
 func (s *Server) UndeleteDomain(ctx context.Context, in *pb.UndeleteDomainRequest) (*google_protobuf.Empty, error) {
-	if err := s.domains.SetDelete(ctx, in.GetDomainId(), false); err != nil {
-		return nil, err
-	}
-	return &google_protobuf.Empty{}, nil
+	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
--- core/adminserver/admin_server_test.go
+++ core/adminserver/admin_server_test.go
@@ -203,3 +203,112 @@ func TestCreateRead(t *testing.T) {
 		}
 	}
 }
+
+func TestDelete(t *testing.T) {
+	testdb.SkipIfNoMySQL(t)
+	ctx := context.Background()
+	storage := fake.NewDomainStorage()
+
+	// Map server
+	mapEnv, err := integration.NewMapEnv(ctx)
+	if err != nil {
+		t.Fatalf("Failed to create trillian map server: %v", err)
+	}
+
+	// Log server
+	numSequencers := 1
+	unused := ""
+	logEnv, err := integration.NewLogEnv(ctx, numSequencers, unused)
+	if err != nil {
+		t.Fatalf("Failed to create trillian log server: %v", err)
+	}
+
+	svr := New(logEnv.Log, mapEnv.Map, logEnv.Admin, mapEnv.Admin, storage, vrfKeyGen)
+
+	for _, tc := range []struct {
+		domainID                 string
+		minInterval, maxInterval time.Duration
+	}{
+		{
+			domainID:    "testdomain",
+			minInterval: 1 * time.Second,
+			maxInterval: 5 * time.Second,
+		},
+	} {
+		if _, err := svr.CreateDomain(ctx, &pb.CreateDomainRequest{
+			DomainId:    tc.domainID,
+			MinInterval: ptypes.DurationProto(tc.minInterval),
+			MaxInterval: ptypes.DurationProto(tc.maxInterval),
+		}); err != nil {
+			t.Fatalf("CreateDomain(): %v", err)
+		}
+		if _, err := svr.DeleteDomain(ctx, &pb.DeleteDomainRequest{DomainId: tc.domainID}); err != nil {
+			t.Fatalf("DeleteDomain(): %v", err)
+		}
+		domain, err := svr.GetDomain(ctx, &pb.GetDomainRequest{DomainId: tc.domainID, ShowDeleted: true})
+		if err != nil {
+			t.Fatalf("GetDomain(): %v", err)
+		}
+		if got, want := domain.Log.Deleted, true; got != want {
+			t.Errorf("Log.TreeState: %v, want %v", got, want)
+		}
+		if got, want := domain.Map.Deleted, true; got != want {
+			t.Errorf("Map.TreeState: %v, want %v", got, want)
+		}
+	}
+}
+
+func TestListDomains(t *testing.T) {
+	testdb.SkipIfNoMySQL(t)
+	ctx := context.Background()
+	storage := fake.NewDomainStorage()
+
+	// Map server
+	mapEnv, err := integration.NewMapEnv(ctx)
+	if err != nil {
+		t.Fatalf("Failed to create trillian map server: %v", err)
+	}
+
+	// Log server
+	numSequencers := 1
+	unused := ""
+	logEnv, err := integration.NewLogEnv(ctx, numSequencers, unused)
+	if err != nil {
+		t.Fatalf("Failed to create trillian log server: %v", err)
+	}
+
+	svr := New(logEnv.Log, mapEnv.Map, logEnv.Admin, mapEnv.Admin, storage, vrfKeyGen)
+
+	for _, tc := range []struct {
+		domainIDs []string
+	}{
+		{
+			domainIDs: []string{"A", "B"},
+		},
+	} {
+		for _, domain := range tc.domainIDs {
+			if _, err := svr.CreateDomain(ctx, &pb.CreateDomainRequest{
+				DomainId:    domain,
+				MinInterval: ptypes.DurationProto(60 * time.Hour),
+				MaxInterval: ptypes.DurationProto(60 * time.Hour),
+			}); err != nil {
+				t.Fatalf("CreateDomain(%v): %v", domain, err)
+			}
+		}
+		resp, err := svr.ListDomains(ctx, &pb.ListDomainsRequest{})
+		if err != nil {
+			t.Fatalf("ListDomains(): %v", err)
+		}
+		// Make sure we got all the same domains back.
+		want := make(map[string]bool)
+		for _, d := range tc.domainIDs {
+			want[d] = true
+		}
+		for _, d := range resp.GetDomains() {
+			domainID := d.GetDomainId()
+			if ok := want[domainID]; !ok {
+				t.Errorf("Did not find domain %v in list response", domainID)
+			}
+		}
+	}
+}
