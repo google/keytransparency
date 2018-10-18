947318d396f534669fe102bd5bcb293de57ddac4
Gary Belvin <gdb@google.com>
2018-10-18 20:10:02 +0100

    Descriptions for list history test

--- core/integration/client_tests.go
+++ core/integration/client_tests.go
@@ -216,41 +216,40 @@ func TestListHistory(ctx context.Context, env *Env, t *testing.T) {
 	}
 
 	for _, tc := range []struct {
+		desc        string
 		start, end  int64
 		wantHistory [][]byte
 		wantErr     bool
 	}{
-		{-1, 1, [][]byte{}, true},                                                        // start epoch < 0: expect error
-		{1, 2, [][]byte{}, false},                                                        // no profile yet
-		{2, 3, [][]byte{cp(1)}, false},                                                   // single profile (first entry at 3)
-		{3, 3, [][]byte{cp(1)}, false},                                                   // single profile (first entry at 3)
-		{4, 4, [][]byte{cp(2)}, false},                                                   // single (changed) profile
-		{5, 5, [][]byte{cp(2)}, false},                                                   // single (unchanged) profile
-		{6, 6, [][]byte{cp(2)}, false},                                                   // single (unchanged) profile
-		{7, 7, [][]byte{cp(3)}, false},                                                   // single (changed) profile
-		{3, 4, [][]byte{cp(1), cp(2)}, false},                                            // multiple profiles
-		{1, 4, [][]byte{cp(1), cp(2)}, false},                                            // test 'nil' first profile(s)
-		{3, 10, [][]byte{cp(1), cp(2), cp(3), cp(4), cp(5)}, false},                      // filtering
-		{9, 16, [][]byte{cp(4), cp(5), cp(6)}, false},                                    // filtering consecutive resubmitted profiles
-		{9, 19, [][]byte{cp(4), cp(5), cp(6), cp(5), cp(7)}, false},                      // no filtering of resubmitted profiles
-		{1, 19, [][]byte{cp(1), cp(2), cp(3), cp(4), cp(5), cp(6), cp(5), cp(7)}, false}, // multiple pages
-		{1, 1001, [][]byte{}, true},                                                      // Invalid end epoch, beyond current epoch
+		{desc: "negative start", start: -1, end: 1, wantHistory: [][]byte{}, wantErr: true},
+		{desc: "large start", start: 1, end: 1001, wantHistory: [][]byte{}, wantErr: true},
+		{desc: "single1", start: 3, end: 3, wantHistory: [][]byte{cp(1)}},
+		{desc: "single3", start: 7, end: 7, wantHistory: [][]byte{cp(3)}},
+		{desc: "single3", start: 8, end: 8, wantHistory: [][]byte{cp(3)}},
+		{desc: "0to3", end: 3, wantHistory: [][]byte{cp(1)}},
+		{desc: "multi", end: 4, wantHistory: [][]byte{cp(1), cp(2)}},
+		{desc: "filter nil", end: 11, wantHistory: [][]byte{cp(1), cp(2), cp(3), cp(4), cp(5)}},
+		{desc: "filter dup", start: 9, end: 16, wantHistory: [][]byte{cp(4), cp(5), cp(6)}},
+		{desc: "filter contiguous", start: 9, end: 19, wantHistory: [][]byte{cp(4), cp(5), cp(6), cp(5), cp(7)}},
+		{desc: "multi page", start: 1, end: 19, wantHistory: [][]byte{cp(1), cp(2), cp(3), cp(4), cp(5), cp(6), cp(5), cp(7)}},
 	} {
-		_, resp, err := env.Client.PaginateHistory(ctx, appID, userID, tc.start, tc.end)
-		if got := err != nil; got != tc.wantErr {
-			t.Errorf("ListHistory(%v, %v) failed: %v, wantErr :%v", tc.start, tc.end, err, tc.wantErr)
-		}
-		if err != nil {
-			continue
-		}
-		compressed, err := client.CompressHistory(resp)
-		if err != nil {
-			t.Errorf("CompressHistory(): %v", err)
-		}
+		t.Run(tc.desc, func(t *testing.T) {
+			_, resp, err := env.Client.PaginateHistory(ctx, appID, userID, tc.start, tc.end)
+			if got := err != nil; got != tc.wantErr {
+				t.Errorf("ListHistory(%v, %v) failed: %v, wantErr :%v", tc.start, tc.end, err, tc.wantErr)
+			}
+			if err != nil {
+				return
+			}
+			compressed, err := client.CompressHistory(resp)
+			if err != nil {
+				t.Errorf("CompressHistory(): %v", err)
+			}
 
-		if got := sortHistory(compressed); !reflect.DeepEqual(got, tc.wantHistory) {
-			t.Errorf("ListHistory(%v, %v): %s, want %s", tc.start, tc.end, got, tc.wantHistory)
-		}
+			if got := sortHistory(compressed); !reflect.DeepEqual(got, tc.wantHistory) {
+				t.Errorf("ListHistory(%v, %v): %s, want %s", tc.start, tc.end, got, tc.wantHistory)
+			}
+		})
 	}
 }
 
