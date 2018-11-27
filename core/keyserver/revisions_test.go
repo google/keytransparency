8572f97cdc27cde5f639ff0ea35246a00d88e006
Gary Belvin <gdb@google.com>
2018-11-27 10:40:24 +0000

    Adjust consumer interfaces

--- core/keyserver/revisions_test.go
+++ core/keyserver/revisions_test.go
@@ -69,7 +69,7 @@ func (b batchStorage) ReadBatch(ctx context.Context, dirID string, rev int64) (*
 
 type mutations map[int64][]*mutator.LogMessage // Map of logID to Slice of LogMessages
 
-func (m *mutations) Send(ctx context.Context, dirID string, mutation *pb.EntryUpdate) error {
+func (m *mutations) Send(ctx context.Context, dirID string, mutation ...*pb.EntryUpdate) error {
 	return errors.New("unimplemented")
 }
 
