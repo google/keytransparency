4d699dabccf75c89216db5ed1d23e9305d114e40
Gary Belvin <gdb@google.com>
2018-11-29 21:24:26 +0000

    Listen on port before creating grpc server.
    In order to pass a sequencer client to the sequencer server,
    we need to grpc.Dial *before* the server is running.
    This works because grpc.Dial is nonblocking by default.

--- cmd/keytransparency-sequencer/server.go
+++ cmd/keytransparency-sequencer/server.go
@@ -15,7 +15,6 @@
 package main
 
 import (
-	"flag"
 	"net/http"
 
 	"github.com/google/keytransparency/cmd/serverutil"
@@ -24,39 +23,15 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/reflection"
-
-	pb "github.com/google/keytransparency/core/api/v1/keytransparency_go_proto"
-	_ "github.com/google/trillian/crypto/keys/der/proto"
-	_ "github.com/google/trillian/merkle/coniks"  // Register hasher
-	_ "github.com/google/trillian/merkle/rfc6962" // Register hasher
-	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 )
 
-var (
-	addr        = flag.String("addr", ":8080", "The ip:port to serve on")
-	metricsAddr = flag.String("metrics-addr", ":8081", "The ip:port to publish metrics on")
-	keyFile     = flag.String("tls-key", "genfiles/server.key", "TLS private key file")
-	certFile    = flag.String("tls-cert", "genfiles/server.crt", "TLS cert file")
-)
-
-func startHTTPServer(svr pb.KeyTransparencyAdminServer) *http.Server {
+func startHTTPServer(grpcServer *grpc.Server, addr string, services ...serverutil.RegisterServiceFromEndpoint) *http.Server {
 	// Wire up gRPC and HTTP servers.
-	creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
-	if err != nil {
-		glog.Exitf("Failed to load server credentials %v", err)
-	}
-	grpcServer := grpc.NewServer(
-		grpc.Creds(creds),
-		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
-		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
-	)
 	tcreds, err := credentials.NewClientTLSFromFile(*certFile, "")
 	if err != nil {
 		glog.Exitf("Failed opening cert file %v: %v", *certFile, err)
 	}
-	gwmux, err := serverutil.GrpcGatewayMux(*addr, tcreds,
-		pb.RegisterKeyTransparencyAdminHandlerFromEndpoint)
+	gwmux, err := serverutil.GrpcGatewayMux(addr, tcreds, services...)
 	if err != nil {
 		glog.Exitf("Failed setting up REST proxy: %v", err)
 	}
@@ -72,18 +47,13 @@ func startHTTPServer(svr pb.KeyTransparencyAdminServer) *http.Server {
 		}
 	}()
 
-	pb.RegisterKeyTransparencyAdminServer(grpcServer, svr)
-	reflection.Register(grpcServer)
-	grpc_prometheus.Register(grpcServer)
-	grpc_prometheus.EnableHandlingTimeHistogram()
-
 	server := &http.Server{
-		Addr:    *addr,
+		Addr:    addr,
 		Handler: serverutil.GrpcHandlerFunc(grpcServer, mux),
 	}
 
 	go func() {
-		glog.Infof("Listening on %v", *addr)
+		glog.Infof("Listening on %v", addr)
 		if err := server.ListenAndServeTLS(*certFile, *keyFile); err != nil {
 			glog.Errorf("ListenAndServeTLS: %v", err)
 		}
