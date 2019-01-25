e1de776b36c169efa7ec644be63d6d627e3dffca
bpopovschi <zyqsempai@mail.ru>
2019-01-25 15:51:55 +0200

    Remove deleted status from map and log trees

--- core/adminserver/admin_server_test.go
+++ core/adminserver/admin_server_test.go
@@ -245,17 +245,6 @@ func TestDelete(t *testing.T) {
 		if _, err := svr.DeleteDirectory(ctx, &pb.DeleteDirectoryRequest{DirectoryId: tc.directoryID}); err != nil {
 			t.Fatalf("DeleteDirectory(): %v", err)
 		}
-		directory, err := svr.GetDirectory(ctx, &pb.GetDirectoryRequest{DirectoryId: tc.directoryID, ShowDeleted: true})
-		if err != nil {
-			t.Fatalf("GetDirectory(): %v", err)
-		}
-		// The tree stored in DB is not updated, that's why we get here false
-		if got, want := directory.Log.Deleted, false; got != want {
-			t.Errorf("Log.Deleted: %v, want %v", got, want)
-		}
-		if got, want := directory.Map.Deleted, false; got != want {
-			t.Errorf("Map.Deleted: %v, want %v", got, want)
-		}
 		if _, err := svr.GarbageCollect(ctx, &pb.GarbageCollectRequest{
 			Before: ptypes.TimestampNow(),
 		}); err != nil {
