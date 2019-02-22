de37477898cc602496681e305eb4ca4b06b42efd
Gary Belvin <gdb@google.com>
2019-02-22 11:15:50 +0000

    Separate out IsValidEntry helper

--- core/mutator/mutator.go
+++ core/mutator/mutator.go
@@ -44,10 +44,13 @@ var (
 	ErrUnauthorized = errors.New("mutation: unauthorized")
 )
 
+// VerifyMutationFn verifies that a mutation is internally consistent.
+type VerifyMutationFn func(mutation *pb.SignedEntry) error
+
 // ReduceMutationFn takes the existing mapleaf and a new mutation and returns the new value for that map leaf.
 // ReduceMutationFn verifies that this is a valid mutation for this item.
 // ReduceMutationFn must be idempotent.
-type ReduceMutationFn func(existingMapLeafValue, mutation *pb.SignedEntry, fullCheck bool) (*pb.SignedEntry, error)
+type ReduceMutationFn func(existingMapLeafValue, mutation *pb.SignedEntry) (*pb.SignedEntry, error)
 
 // MapLogItemFn takes a log item and emits 0 or more KV<index, mutations> pairs.
 type MapLogItemFn func(logItem *LogMessage, emit func(index []byte, mutation *pb.EntryUpdate)) error
