a64ea1782a6d83d0e2c32d37fe7e0d3fd2a85bfc
Gary Belvin <gdbelvin@gmail.com>
2019-03-05 11:01:47 +0000

    Emit errors during the mapping phase (#1231)
    * Emit errors during the mapping phase
    * err msg

--- core/mutator/mutator.go
+++ core/mutator/mutator.go
@@ -59,7 +59,8 @@ type ReduceMutationFn func(msgs []*pb.EntryUpdate, leaves []*pb.EntryUpdate,
 	emit func(*pb.EntryUpdate), emitErr func(error))
 
 // MapLogItemFn takes a log item and emits 0 or more KV<index, mutations> pairs.
-type MapLogItemFn func(logItem *LogMessage, emit func(index []byte, mutation *pb.EntryUpdate)) error
+type MapLogItemFn func(logItem *LogMessage,
+	emit func(index []byte, mutation *pb.EntryUpdate), emitErr func(error))
 
 // LogMessage represents a change to a user, and associated data.
 type LogMessage struct {
