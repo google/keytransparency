199fa2629d795641ef06b4e2e21769853b8cc3bd
Gary Belvin <gdb@google.com>
2019-07-18 12:44:13 +0100

    Increase timeouts

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -10,8 +10,8 @@ docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 trap "docker-compose down" INT EXIT
 
 set +e
-wget -q --spider --retry-connrefused --waitretry=1 -t 10 0.0.0.0:443 || sleep 2
-wget -q --spider --retry-on-http-error=404 -t 30 1 0.0.0.0:443/v1/directories/default 
+wget -t 60 --spider --retry-connrefused --waitretry=1  0.0.0.0:443 || sleep 2
+wget -t 60 --spider --retry-on-http-error=404  1 0.0.0.0:443/v1/directories/default
 set -e
 
 PASSWORD="foobar"
