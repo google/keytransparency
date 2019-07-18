0971ab59421fd03a21ffe918f07768ad43de0a2f
Gary Belvin <gdb@google.com>
2019-07-18 17:39:41 +0100

    Nits and simplify

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -10,13 +10,13 @@ docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 trap "docker-compose down" INT EXIT
 
 set +e
-wget -t 60 --spider --retry-connrefused --waitretry=1  0.0.0.0:443 || sleep 2
-wget -t 60 --spider --retry-on-http-error=404  1 0.0.0.0:443/v1/directories/default
+wget -T 60 --spider --retry-connrefused localhost:443
+wget -T 60 --spider --retry-on-http-error=404 --no-check-certificate https://localhost/v1/directories/default
 set -e
 
 PASSWORD="foobar"
-go run ./cmd/keytransparency-client/main.go authorized-keys create-keyset --password=${PASSWORD}
-go run ./cmd/keytransparency-client/main.go post foo@bar.com \
+go run ./cmd/keytransparency-client authorized-keys create-keyset --password=${PASSWORD}
+go run ./cmd/keytransparency-client post foo@bar.com \
 	--insecure \
 	--data='dGVzdA==' \
 	--password=${PASSWORD} \
