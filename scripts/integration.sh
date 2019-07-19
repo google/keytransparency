fd2ea170778a8d984df67f718f0b615709bb94ee
Gary Belvin <gdb@google.com>
2019-07-19 11:15:27 +0100

    respect wget errors

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -9,10 +9,11 @@ fi
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 trap "docker-compose down" INT EXIT
 
-set +e
-wget -T 60 --spider --retry-connrefused localhost:443
-wget -T 60 --spider --retry-on-http-error=404 --no-check-certificate https://localhost/v1/directories/default
-set -e
+wget -T 60 --spider --retry-connrefused --waitretry=1 http://localhost:8081/metrics
+wget -T 60 -O /dev/null --no-check-certificate  \
+	--retry-connrefused --waitretry=1 \
+	--retry-on-http-error=405,404 \
+	https://localhost/v1/directories/default
 
 PASSWORD="foobar"
 go run ./cmd/keytransparency-client authorized-keys create-keyset --password=${PASSWORD}
@@ -20,6 +21,6 @@ go run ./cmd/keytransparency-client post foo@bar.com \
 	--insecure \
 	--data='dGVzdA==' \
 	--password=${PASSWORD} \
-	--kt-url=0.0.0.0:443 \
+	--kt-url=localhost:443 \
 	--verbose \
 	--logtostderr
