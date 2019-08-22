94ae5210ef595f84246b8845302a9032d0395b63
Gary Belvin <gdb@google.com>
2019-08-22 22:52:31 +0100

    Move batch tests to integration

--- core/integration/storagetest/batch.go
+++ core/integration/storagetest/batch.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"testing"
 
-	spb "github.com/google/keytransparency/core/sequencer/sequencer_go_proto"
+	"github.com/golang/protobuf/proto"
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	spb "github.com/google/keytransparency/core/sequencer/sequencer_go_proto"
 )
 
 // Batcher writes batch definitions to storage.
@@ -43,5 +46,80 @@ func (*BatchTests) TestNotFound(ctx context.Context, t *testing.T, b Batcher) {
 	st := status.Convert(err)
 	if got, want := st.Code(), codes.NotFound; got != want {
 		t.Errorf("ReadBatch(): %v, want %v", err, want)
+	}
+}
+
+func (*BatchTests) TestWriteBatch(ctx context.Context, t *testing.T, b Batcher) {
+	domainID := "writebatchtest"
+	for _, tc := range []struct {
+		rev     int64
+		wantErr bool
+		sources []*spb.MapMetadata_SourceSlice
+	}{
+		// Tests are cumulative.
+		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 11}}},
+		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 12}}, wantErr: true},
+		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 21}}, wantErr: true},
+		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{}, wantErr: true},
+		{rev: 1, sources: []*spb.MapMetadata_SourceSlice{}},
+		{rev: 1, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 11}}, wantErr: true},
+	} {
+		err := b.WriteBatchSources(ctx, domainID, tc.rev, &spb.MapMetadata{Sources: tc.sources})
+		if got, want := err != nil, tc.wantErr; got != want {
+			t.Errorf("WriteBatchSources(%v, %v): err: %v. code: %v, want %v",
+				tc.rev, tc.sources, err, got, want)
+		}
+	}
+}
+
+func TestReadBatch(ctx context.Context, t *testing.T, b Batcher) {
+	domainID := "readbatchtest"
+	for _, tc := range []struct {
+		rev  int64
+		want *spb.MapMetadata
+	}{
+		{rev: 0, want: &spb.MapMetadata{Sources: []*spb.MapMetadata_SourceSlice{
+			{LogId: 1, HighestExclusive: 11},
+			{LogId: 2, HighestExclusive: 21},
+		}}},
+		{rev: 1, want: &spb.MapMetadata{Sources: []*spb.MapMetadata_SourceSlice{
+			{LogId: 1, HighestExclusive: 12},
+			{LogId: 2, HighestExclusive: 23},
+		}}},
+	} {
+		if err := b.WriteBatchSources(ctx, domainID, tc.rev, tc.want); err != nil {
+			t.Fatalf("WriteBatch(%v): %v", tc.rev, err)
+		}
+		got, err := b.ReadBatch(ctx, domainID, tc.rev)
+		if err != nil {
+			t.Fatalf("ReadBatch(%v): %v", tc.rev, err)
+		}
+		if !cmp.Equal(got, tc.want, cmp.Comparer(proto.Equal)) {
+			t.Errorf("ReadBatch(%v): %v, want %v", tc.rev, got, tc.want)
+		}
+	}
+}
+
+func TestHightestRev(ctx context.Context, t *testing.T, b Batcher) {
+	domainID := "writebatchtest"
+	for _, tc := range []struct {
+		rev     int64
+		sources []*spb.MapMetadata_SourceSlice
+	}{
+		// Tests are cumulative.
+		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 11}}},
+		{rev: 1, sources: []*spb.MapMetadata_SourceSlice{}},
+	} {
+		err := b.WriteBatchSources(ctx, domainID, tc.rev, &spb.MapMetadata{Sources: tc.sources})
+		if err != nil {
+			t.Errorf("WriteBatchSources(%v, %v): err: %v", tc.rev, tc.sources, err)
+		}
+		got, err := b.HighestRev(ctx, domainID)
+		if err != nil {
+			t.Errorf("HighestRev(): %v", err)
+		}
+		if got != tc.rev {
+			t.Errorf("HighestRev(): %v, want %v", got, tc.rev)
+		}
 	}
 }
--- impl/sql/mutationstorage/mutations_test.go
+++ impl/sql/mutationstorage/mutations_test.go
@@ -18,15 +18,10 @@ import (
 	"database/sql"
 	"testing"
 
-	"github.com/golang/protobuf/proto"
-	"github.com/google/go-cmp/cmp"
 	"github.com/google/keytransparency/core/integration/storagetest"
 
-	spb "github.com/google/keytransparency/core/sequencer/sequencer_go_proto"
 	_ "github.com/mattn/go-sqlite3"
 )
-
-const directoryID = "default"
 
 func newDB(t testing.TB) *sql.DB {
 	db, err := sql.Open("sqlite3", ":memory:")
@@ -46,100 +41,4 @@ func TestBatchIntegration(t *testing.T) {
 	}
 
 	storagetest.RunBatchStorageTests(t, storageFactory)
-}
-
-func TestWriteBatch(t *testing.T) {
-	ctx := context.Background()
-	db := newDB(t)
-	m, err := New(db)
-	if err != nil {
-		t.Fatalf("Failed to create mutations: %v", err)
-	}
-
-	domainID := "writebatchtest"
-	for _, tc := range []struct {
-		rev     int64
-		wantErr bool
-		sources []*spb.MapMetadata_SourceSlice
-	}{
-		// Tests are cumulative.
-		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 11}}},
-		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 12}}, wantErr: true},
-		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 21}}, wantErr: true},
-		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{}, wantErr: true},
-		{rev: 1, sources: []*spb.MapMetadata_SourceSlice{}},
-		{rev: 1, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 11}}, wantErr: true},
-	} {
-		err := m.WriteBatchSources(ctx, domainID, tc.rev, &spb.MapMetadata{Sources: tc.sources})
-		if got, want := err != nil, tc.wantErr; got != want {
-			t.Errorf("WriteBatchSources(%v, %v): err: %v. code: %v, want %v",
-				tc.rev, tc.sources, err, got, want)
-		}
-	}
-}
-
-func TestReadBatch(t *testing.T) {
-	ctx := context.Background()
-	db := newDB(t)
-	m, err := New(db)
-	if err != nil {
-		t.Fatalf("Failed to create mutations: %v", err)
-	}
-
-	domainID := "readbatchtest"
-	for _, tc := range []struct {
-		rev  int64
-		want *spb.MapMetadata
-	}{
-		{rev: 0, want: &spb.MapMetadata{Sources: []*spb.MapMetadata_SourceSlice{
-			{LogId: 1, HighestExclusive: 11},
-			{LogId: 2, HighestExclusive: 21},
-		}}},
-		{rev: 1, want: &spb.MapMetadata{Sources: []*spb.MapMetadata_SourceSlice{
-			{LogId: 1, HighestExclusive: 12},
-			{LogId: 2, HighestExclusive: 23},
-		}}},
-	} {
-		if err := m.WriteBatchSources(ctx, domainID, tc.rev, tc.want); err != nil {
-			t.Fatalf("WriteBatch(%v): %v", tc.rev, err)
-		}
-		got, err := m.ReadBatch(ctx, domainID, tc.rev)
-		if err != nil {
-			t.Fatalf("ReadBatch(%v): %v", tc.rev, err)
-		}
-		if !cmp.Equal(got, tc.want, cmp.Comparer(proto.Equal)) {
-			t.Errorf("ReadBatch(%v): %v, want %v", tc.rev, got, tc.want)
-		}
-	}
-}
-
-func TestHightestRev(t *testing.T) {
-	ctx := context.Background()
-	db := newDB(t)
-	m, err := New(db)
-	if err != nil {
-		t.Fatalf("Failed to create mutations: %v", err)
-	}
-
-	domainID := "writebatchtest"
-	for _, tc := range []struct {
-		rev     int64
-		sources []*spb.MapMetadata_SourceSlice
-	}{
-		// Tests are cumulative.
-		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 11}}},
-		{rev: 1, sources: []*spb.MapMetadata_SourceSlice{}},
-	} {
-		err := m.WriteBatchSources(ctx, domainID, tc.rev, &spb.MapMetadata{Sources: tc.sources})
-		if err != nil {
-			t.Errorf("WriteBatchSources(%v, %v): err: %v", tc.rev, tc.sources, err)
-		}
-		got, err := m.HighestRev(ctx, domainID)
-		if err != nil {
-			t.Errorf("HighestRev(): %v", err)
-		}
-		if got != tc.rev {
-			t.Errorf("HighestRev(): %v, want %v", got, tc.rev)
-		}
-	}
 }
