3b2fe49744eb82e4dab6b38d537177bb3af65f0f
Gary Belvin <gdb@google.com>
2019-11-01 18:35:10 +0000

    Memory impl of mutation logs

---
+++ core/integration/memory/mutation_logs.go
@@ -0,0 +1,107 @@
+// Copyright 2019 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package memory supplies fake in-memory implementations for testing purposes.
+package memory
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"time"
+
+	"github.com/google/keytransparency/core/mutator"
+
+	pb "github.com/google/keytransparency/core/api/v1/keytransparency_go_proto"
+)
+
+// NewMutationLog creates a new fake MutationLog
+func NewMutationLog() MutationLog {
+	return make(MutationLog)
+}
+
+// MutationLog is a fake implementation of keyserver.
+type batch struct {
+	time time.Time
+	msgs []*mutator.LogMessage
+}
+
+// MutationLog is a fake, in-memory implementation of keyserver.MutationLogs
+type MutationLog map[int64][]batch // Map of logID to Slice of LogMessages
+
+// AddLogs to the mutation database.
+func (m MutationLog) AddLogs(ctx context.Context, directoryID string, logIDs ...int64) error {
+	for _, logID := range logIDs {
+		m[logID] = make([]batch, 0)
+	}
+	return nil
+}
+
+// Send a batch of mutations to a random log.
+func (m MutationLog) Send(ctx context.Context, dirID string, mutation ...*pb.EntryUpdate) (int64, time.Time, error) {
+	// Select a random logID
+	var logID int64
+	for i := range m {
+		logID = i
+		break
+	}
+	ts := time.Now()
+	// Remove the PII contained in EntryUpdate. Only save the merkle tree bits.
+	entries := make([]*pb.SignedEntry, 0, len(mutation))
+	for _, i := range mutation {
+		entries = append(entries, i.Mutation)
+	}
+	m.SendAt(logID, ts, entries)
+	return logID, ts, nil
+}
+
+// SendAt sends a batch of mutations with a given timestamp.
+func (m MutationLog) SendAt(logID int64, ts time.Time, entries []*pb.SignedEntry) {
+	logShard := m[logID]
+	if len(logShard) > 0 && logShard[len(logShard)-1].time.After(ts) {
+		panic(fmt.Sprintf("inserting mutation entry %v out of order", ts))
+	}
+
+	// Convert []SignedEntry into []LogMessage for storage.
+	msgs := make([]*mutator.LogMessage, 0, len(entries))
+	for _, e := range entries {
+		msgs = append(msgs, &mutator.LogMessage{ID: ts, Mutation: e})
+	}
+	m[logID] = append(logShard, batch{time: ts, msgs: msgs})
+}
+
+// ReadLog returns mutations between low, and high. Always returns complete batches.
+func (m MutationLog) ReadLog(ctx context.Context, dirID string,
+	logID int64, low, high time.Time, batchSize int32) ([]*mutator.LogMessage, error) {
+	logShard := m[logID]
+	if len(logShard) == 0 || batchSize == 0 {
+		return nil, nil
+	}
+	start := int(sort.Search(len(logShard), func(i int) bool { return !logShard[i].time.Before(low) }))
+	end := int(sort.Search(len(logShard), func(i int) bool { return !logShard[i].time.Before(high) }))
+	// If the search is unsuccessful, i will be equal to len(logShard).
+	if start == len(logShard) && logShard[start].time.Before(low) {
+		return nil, fmt.Errorf("invalid argument: low: %v, want <= max watermark: %v", low, logShard[start].time)
+	}
+	out := make([]*mutator.LogMessage, 0, batchSize)
+	count := int32(0)
+	for i := start; i < end; i++ {
+		out = append(out, logShard[i].msgs...)
+		count += int32(len(logShard[i].msgs))
+		if count >= batchSize {
+			break
+		}
+	}
+	return out, nil
+}
