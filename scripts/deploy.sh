f37d7275d2a1c3abc666ddc56fc771d0319b0116
Gary Belvin <gdb@google.com>
2020-01-06 14:16:31 +0000

    Split kt-secrets into kt-tls and kt-monitor
    - Move Monitor key to it's own secret
    - Move the TLS keys to a TLS secret

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -37,11 +37,12 @@ gcloud --quiet auth configure-docker
 test $(basename $(pwd)) == "keytransparency" || exit 1
 
 # kubectl exits with 1 if kt-secret does not exist
-if ! kubectl get secret kt-secrets; then
+if ! kubectl get secret kt-tls; then
   echo "Generating keys..."
   rm -f ./genfiles/*
   ./scripts/prepare_server.sh -f
-  kubectl create secret generic kt-secrets --from-file=genfiles/server.crt --from-file=genfiles/server.key --from-file=genfiles/monitor_sign-key.pem
+  kubectl create secret generic kt-monitor --from-file=genfiles/monitor_sign-key.pem
+  kubectl create secret tls kt-tls --cert=genfiles/server.crt --key=genfiles/server.key
 fi
 
 echo "Building docker images..."
