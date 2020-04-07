0cfc2697c7c8757df556e0bb173877f313e29af6
Gary Belvin <gdb@google.com>
2020-04-07 12:59:00 +0100

    EncodeDecode test

--- core/crypto/draft-irtf-cfrg-vrf-06/conversion_test.go
+++ core/crypto/draft-irtf-cfrg-vrf-06/conversion_test.go
@@ -2,6 +2,8 @@ package vrf
 
 import (
 	"bytes"
+	"crypto/elliptic"
+	"crypto/rand"
 	"fmt"
 	"math/big"
 	"testing"
@@ -32,5 +34,26 @@ func TestI2OSP(t *testing.T) {
 				t.Errorf("I2OSP(%v, %v): %v, want %v", tc.x, tc.xLen, got, tc.want)
 			}
 		})
+	}
+}
+
+func TestSEG1EncodeDecode(t *testing.T) {
+	c := elliptic.P256()
+	_, Ax, Ay, err := elliptic.GenerateKey(c, rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b := secg1EncodeCompressed(c, Ax, Ay)
+	Bx, By := secg1Decode(c, b)
+
+	if Bx == nil || By == nil {
+		t.Fatalf("SEG1Decode returned nil")
+	}
+	if Bx.Cmp(Ax) != 0 {
+		t.Fatalf("Bx: %v, want %v", Bx, Ax)
+	}
+	if By.Cmp(Ay) != 0 {
+		t.Fatalf("By: %v, want %v", By, Ay)
 	}
 }
