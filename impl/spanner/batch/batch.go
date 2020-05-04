15bb625fb7fdd348f25dab56b7405ed16e544056
Gary Belvin <gdb@google.com>
2020-05-04 16:31:30 +0100

    Batch table

---
+++ impl/spanner/batch/batch.go
@@ -0,0 +1,99 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package batch allows for reading and writing of batches of mutations.
+// Each batch of revisions corresponds to a map revision.
+package batch
+
+import (
+	"context"
+
+	"cloud.google.com/go/spanner"
+	"github.com/golang/protobuf/proto"
+
+	spb "github.com/google/keytransparency/core/sequencer/sequencer_go_proto"
+)
+
+const table = "Batches"
+
+// Table implements sequencer.Batcher
+type Table struct {
+	client *spanner.Client
+}
+
+// New returns a new Table.
+func New(client *spanner.Client) *Table {
+	return &Table{client: client}
+}
+
+// WriteBatchSources saves the (low, high] boundaries used for each log in making this revision.
+func (t *Table) WriteBatchSources(ctx context.Context, dirID string, rev int64, sources *spb.MapMetadata) error {
+	metaBytes, err := proto.Marshal(sources)
+	if err != nil {
+		return err
+	}
+
+	// Cols are columns of the Batches table.
+	type Cols struct {
+		DirectoryID string
+		Revision    int64
+		Meta        []byte
+	}
+	m, err := spanner.InsertStruct(table, Cols{
+		DirectoryID: dirID,
+		Revision:    rev,
+		Meta:        metaBytes,
+	})
+	if err != nil {
+		return err
+	}
+	_, err = t.client.Apply(ctx, []*spanner.Mutation{m})
+	return err
+}
+
+// ReadBatch returns the batch definitions for a given revision.
+func (t *Table) ReadBatch(ctx context.Context, directoryID string, rev int64) (*spb.MapMetadata, error) {
+	rtx := t.client.Single()
+	defer rtx.Close()
+
+	var metaBytes []byte
+	r, err := rtx.ReadRow(ctx, table, spanner.Key{directoryID, rev}, []string{"Meta"})
+	if err != nil {
+		return nil, err
+	}
+	if err := r.ColumnByName("Meta", &metaBytes); err != nil {
+		return nil, err
+	}
+	meta := spb.MapMetadata{}
+	if err := proto.Unmarshal(metaBytes, &meta); err != nil {
+		return nil, err
+	}
+	return &meta, nil
+}
+
+// HighestRev returns the highest defined revision number for directoryID.
+func (t *Table) HighestRev(ctx context.Context, directoryID string) (int64, error) {
+	rtx := t.client.Single()
+	defer rtx.Close()
+
+	// TODO: Replace with MAX(Revision) when spansql supports aggregate operators.
+	stmt := spanner.NewStatement(`SELECT Revision FROM Batches WHERE DirectoryID = @directoryID ORDER BY Revision DESC LIMIT 1`)
+	stmt.Params["directoryID"] = directoryID
+	var rev int64
+	err := rtx.Query(ctx, stmt).Do(
+		func(row *spanner.Row) error {
+			return row.Columns(&rev)
+		})
+	return rev, err
+}
---
+++ impl/spanner/batch/batch_test.go
@@ -0,0 +1,160 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package batch
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/keytransparency/core/integration/storagetest"
+	"github.com/google/keytransparency/core/sequencer"
+	"github.com/google/keytransparency/impl/spanner/directory"
+	"github.com/google/keytransparency/impl/spanner/testutil"
+	"github.com/google/trillian/crypto/keyspb"
+
+	dtype "github.com/google/keytransparency/core/directory"
+	spb "github.com/google/keytransparency/core/sequencer/sequencer_go_proto"
+	ktspanner "github.com/google/keytransparency/impl/spanner"
+	tpb "github.com/google/trillian"
+)
+
+func NewForTest(ctx context.Context, t *testing.T, dirID string) (*Table, func()) {
+	t.Helper()
+	ddl, err := ktspanner.ReadDDL()
+	if err != nil {
+		t.Fatal(err)
+	}
+	client, cleanup := testutil.CreateDatabase(ctx, t, ddl)
+	b := New(client)
+
+	if err := directory.New(client).Write(ctx, &dtype.Directory{
+		DirectoryID: dirID,
+		Map:         &tpb.Tree{},
+		Log:         &tpb.Tree{},
+		VRFPriv:     &keyspb.PrivateKey{Der: []byte("privkeybytes")},
+	}); err != nil {
+		t.Fatalf("directories.Write(%v): %v", dirID, err)
+	}
+
+	return b, cleanup
+}
+
+func TestNewForTest(t *testing.T) {
+	ctx := context.Background()
+	directoryID := "new"
+	_, done := NewForTest(ctx, t, directoryID)
+	defer done()
+}
+
+func TestBatchIntegration(t *testing.T) {
+	storageFactory :=
+		func(ctx context.Context, t *testing.T, dirID string) (sequencer.Batcher, func(context.Context)) {
+			b, done := NewForTest(ctx, t, dirID)
+			return b, func(_ context.Context) { done() }
+		}
+
+	storagetest.RunBatchStorageTests(t, storageFactory)
+}
+
+func TestWriteBatch(t *testing.T) {
+	ctx := context.Background()
+	directoryID := "writebatch"
+	m, done := NewForTest(ctx, t, directoryID)
+	defer done()
+	for _, tc := range []struct {
+		rev     int64
+		wantErr bool
+		sources []*spb.MapMetadata_SourceSlice
+	}{
+		// Tests are cumulative.
+		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 10}}},
+		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 11}}, wantErr: true},
+		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 2, HighestExclusive: 20}}, wantErr: true},
+		{rev: 0, sources: []*spb.MapMetadata_SourceSlice{}, wantErr: true},
+		{rev: 1, sources: []*spb.MapMetadata_SourceSlice{}},
+		{rev: 1, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 10}}, wantErr: true},
+	} {
+		err := m.WriteBatchSources(ctx, directoryID, tc.rev, &spb.MapMetadata{Sources: tc.sources})
+		if got, want := err != nil, tc.wantErr; got != want {
+			t.Errorf("WriteBatchSources(%v, %v): err: %v. code: %v, want %v",
+				tc.rev, tc.sources, err, got, want)
+		}
+	}
+}
+
+func TestReadBatch(t *testing.T) {
+	ctx := context.Background()
+	directoryID := "readbatch"
+	m, done := NewForTest(ctx, t, directoryID)
+	defer done()
+
+	for _, tc := range []struct {
+		rev  int64
+		want *spb.MapMetadata
+	}{
+		{rev: 0, want: &spb.MapMetadata{Sources: []*spb.MapMetadata_SourceSlice{
+			{LogId: 1, HighestExclusive: 10},
+			{LogId: 2, HighestExclusive: 20},
+		}}},
+		{rev: 1, want: &spb.MapMetadata{Sources: []*spb.MapMetadata_SourceSlice{
+			{LogId: 1, HighestExclusive: 11},
+			{LogId: 2, HighestExclusive: 22},
+		}}},
+	} {
+		if err := m.WriteBatchSources(ctx, directoryID, tc.rev, tc.want); err != nil {
+			t.Fatalf("WriteBatch(%v): %v", tc.rev, err)
+		}
+		got, err := m.ReadBatch(ctx, directoryID, tc.rev)
+		if err != nil {
+			t.Fatalf("ReadBatch(%v): %v", tc.rev, err)
+		}
+		if !cmp.Equal(got, tc.want, cmp.Comparer(proto.Equal)) {
+			t.Errorf("ReadBatch(%v): %v, want %v", tc.rev, got, tc.want)
+		}
+	}
+}
+
+func TestHightestRev(t *testing.T) {
+	ctx := context.Background()
+	directoryID := "highestrev"
+	m, done := NewForTest(ctx, t, directoryID)
+	defer done()
+
+	for _, tc := range []struct {
+		desc    string
+		rev     int64
+		sources []*spb.MapMetadata_SourceSlice
+	}{
+		// Tests are cumulative.
+		{desc: "rev0", rev: 0, sources: []*spb.MapMetadata_SourceSlice{{LogId: 1, HighestExclusive: 10}}},
+		{desc: "rev1", rev: 1, sources: []*spb.MapMetadata_SourceSlice{}},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := m.WriteBatchSources(ctx, directoryID, tc.rev, &spb.MapMetadata{Sources: tc.sources})
+			if err != nil {
+				t.Errorf("WriteBatchSources(%v, %v): err: %v", tc.rev, tc.sources, err)
+			}
+			got, err := m.HighestRev(ctx, directoryID)
+			if err != nil {
+				t.Errorf("HighestRev(): %v", err)
+			}
+			if got != tc.rev {
+				t.Errorf("HighestRev(): %v, want %v", got, tc.rev)
+			}
+		})
+	}
+}
