14afe2ed1eae4ec51488e0f85167e46d7a5b61e1
Gary Belvin <gdb@google.com>
2020-05-14 10:14:55 +0100

    Check close errors

--- impl/spanner/testutil/testutil.go
+++ impl/spanner/testutil/testutil.go
@@ -66,7 +66,7 @@ func inMemClient(ctx context.Context, t testing.TB, dbName string) (*spanner.Cli
 	if err != nil {
 		t.Fatalf("Dialing in-memory fake: %v", err)
 	}
-	t.Cleanup(func() { conn.Close() })
+	t.Cleanup(func() { conn.Close() /* May already be closed by adminClient */ })
 	client, err := spanner.NewClient(ctx, dbName, option.WithGRPCConn(conn))
 	if err != nil {
 		t.Fatalf("Connecting to in-memory fake: %v", err)
@@ -76,7 +76,11 @@ func inMemClient(ctx context.Context, t testing.TB, dbName string) (*spanner.Cli
 	if err != nil {
 		t.Fatalf("Connecting to in-memory fake DB admin: %v", err)
 	}
-	t.Cleanup(func() { adminClient.Close() })
+	t.Cleanup(func() {
+		if err := adminClient.Close(); err != nil {
+			t.Error(err)
+		}
+	})
 
 	return client, adminClient
 }
