4e6e34931183a736f16bc496feec349ae858d84e
Gary Belvin <gdb@google.com>
2020-06-05 15:04:28 +0100

    Fix package doc string

--- impl/mysql/open.go
+++ impl/mysql/open.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package sql provides functions for interacting with MySQL.
+// Package mysql provides functions for interacting with MySQL.
 package mysql
 
 import (
